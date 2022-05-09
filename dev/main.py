0510ff543a76e7a262e29ba5a7ec9079183e9671
dzhavid <fataliev.ds@phystech.edu>
2022-05-09 16:45:05 +0300

    review

--- dev/main.py
+++ dev/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import argparse
 
 
 def shift(c, dd):
@@ -9,23 +10,15 @@ def shift(c, dd):
     return chr((ord(c) - base_ind + dd) % 26 + base_ind)
 
 
-def shift_text_vigenere(text, k):
+def shift_text(text, k, type):
     result_text = ['a' for i in range(len(text))]
     for i in range(len(text)):
-        result_text[i] = shift(text[i], ((ord(k[i % len(k)]) - ord('a')) % 26))
-    return ''.join(result_text)
-
-
-def decode_vigenere(text, k):
-    result_text = ['a' for i in range(len(text))]
-    for i in range(len(text)):
-        result_text[i] = shift(text[i], 26 - ((ord(k[i % len(k)]) - ord('a')) % 26))
-    return ''.join(result_text)
-
-def shift_text_caesar(text, k):
-    result_text = ['a' for i in range(len(text))]
-    for i in range(len(text)):
-        result_text[i] = shift(text[i], k)
+        if type == "stc":
+            result_text[i] = shift(text[i], k)
+        if type == "dv":
+            result_text[i] = shift(text[i], 26 - ((ord(k[i % len(k)]) - ord('a')) % 26))
+        if type == "stv":
+            result_text[i] = shift(text[i], ((ord(k[i % len(k)]) - ord('a')) % 26))
     return ''.join(result_text)
 
 
@@ -42,10 +35,10 @@ def count_frequencies(text):
     return frequencies
 
 
-def L2_MSE(frequencies1, frequencies2):
+def MAE(frequencies1, frequencies2):
     error = 0
     for i in range(26):
-        error += abs(frequencies1[i] - frequencies2[i])
+        error += abs(frequencies1[i] - frequencies2[i]) / 26
     return error
 
 
@@ -53,11 +46,11 @@ def decode(text, sample):
     min_error = 10000
     min_k = 30
     for k in range(26):
-        freqs = count_frequencies(shift_text_caesar(text, 26 - k))
-        if L2_MSE(freqs, sample) < min_error:
-            min_error = L2_MSE(freqs, sample)
+        freqs = count_frequencies(shift_text(text, 26 - k, "stc"))
+        if MAE(freqs, sample) < min_error:
+            min_error = MAE(freqs, sample)
             min_k = k
-    return shift_text_caesar(text, 26 - min_k)
+    return shift_text(text, 26 - min_k, "stc")
 
 
 f = open('War_and_Peace.txt')
@@ -65,57 +58,67 @@ sample_text = f.read()
 f.close()
 sample_freqs = count_frequencies(sample_text)
 
-if sys.argv[1] == "encode":
-    if sys.argv[3] == "caesar":
-        k = int(sys.argv[5])
+
+pasrsing = argparse.ArgumentParser()
+pasrsing.add_argument("action", type=str, help="decode/encode/decode-without-key")
+pasrsing.add_argument("--cipher", type=str, help="caesar/vigenere")
+pasrsing.add_argument("--key", type=str)
+pasrsing.add_argument("--input_file", type=str)
+pasrsing.add_argument("--output_file", type=str)
+arguments = pasrsing.parse_args()
+
+
+if arguments.action == "encode":
+    if arguments.cipher == "caesar":
+        k = int(arguments.key)
         if len(sys.argv) == 6:
             input_txt = input()
-            print(shift_text_caesar(input_txt, k))
+            print(shift_text(input_txt, k, "stc"))
         else:
-            input_1 = open(sys.argv[7])
+            input_1 = open(arguments.input_file)
             input_txt = input_1.read()
             input_1.close()
-            output_1 = open(sys.argv[9], "w")
-            output_1.write(shift_text_caesar(input_txt, k))
+            output_1 = open(arguments.output_file, "w")
+            output_1.write(shift_text(input_txt, k, "stc"))
             output_1.close()
     else:
-        k = sys.argv[5]
+        k = arguments.key
         if len(sys.argv) == 6:
             input_txt = input()
-            print(shift_text_vigenere(input_txt, k))
+            print(shift_text(input_txt, k, "stv"))
         else:
-            input_1 = open(sys.argv[7])
+            input_1 = open(arguments.input_file)
             input_txt = input_1.read()
             input_1.close()
-            output_1 = open(sys.argv[9], "w")
-            output_1.write(shift_text_vigenere(input_txt, k))
+            output_1 = open(arguments.output_file, "w")
+            output_1.write(shift_text(input_txt, k, "stv"))
             output_1.close()
-if sys.argv[1] == "decode":
-    if sys.argv[3] == "caesar":
-        k = int(sys.argv[5])
+if arguments.action == "decode":
+    if arguments.cipher == "caesar":
+        k = int(arguments.key)
         if len(sys.argv) == 6:
             input_txt = input()
-            print(shift_text_caesar(input_txt, 26 - k))
+            print(shift_text(input_txt, 26 - k, "stc"))
         else:
-            input_1 = open(sys.argv[7])
+            input_1 = open(arguments.input_file)
             input_txt = input_1.read()
             input_1.close()
-            output_1 = open(sys.argv[9], "w")
-            output_1.write(shift_text_caesar(input_txt, 26 - k))
+            output_1 = open(arguments.output_file, "w")
+            output_1.write(shift_text(input_txt, 26 - k, "stc"))
             output_1.close()
     else:
-        k = sys.argv[5]
+        k = arguments.key
         if len(sys.argv) == 6:
             input_txt = input()
-            print(decode_vigenere(input_txt, k))
+            print(shift_text(input_txt, k, "dv"))
         else:
-            input_1 = open(sys.argv[7])
+            input_1 = open(arguments.input_file)
             input_txt = input_1.read()
             input_1.close()
-            output_1 = open(sys.argv[9], "w")
-            output_1.write(decode_vigenere(input_txt, k))
+            output_1 = open(arguments.output_file, "w")
+            output_1.write(shift_text(input_txt, k, "dv"))
             output_1.close()
-if sys.argv[1] == "decode-without-key":
+if arguments.action == "decode-without-key":
     if len(sys.argv) == 2:
         input_txt = input()
         print(decode(input_txt, sample_freqs))
